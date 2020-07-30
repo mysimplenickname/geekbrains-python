cffdbe112461c5d883db1b6723dedf8d3674aaf1
mysimplenickname <lvmlkv@yandex.ru>
2020-07-30 22:30:49 +0300

    replace a simple function with a lambda

--- homework4/5.py
+++ homework4/5.py
@@ -1,11 +1,6 @@
 from functools import reduce
 
-
-def multiply_func(a, b):
-    return a * b
-
-
 initial_list = [i for i in range(100, 1001) if i % 2 == 0]
 
 print(f"Initial list: {initial_list}")
-print(f"Result: {reduce(multiply_func, initial_list)}")
+print(f"Result: {reduce(lambda x, y: x * y, initial_list)}")
